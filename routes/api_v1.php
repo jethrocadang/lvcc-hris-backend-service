6e4ff82028e543ef4b0ca658c63d2a59595c9d06
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2025-02-01 11:23:09 +0800

    chore: clean refactor routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,11 +3,23 @@
 use App\Http\Controllers\Api\V1\OauthController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(OauthController::class)->group(function () {
-    Route::post('auth/google-callback', 'googleAuthentication');
-    Route::get('auth/test', [OauthController::class, 'test']);
-});
+/**
+ * ==============================
+ *  Test Routes - For API checking
+ * ==============================
+ */
 
 Route::get('test', function () {
     return 'test - API v1';
+});
+
+/**
+ * ==============================
+ *  Authentication Routes (JWT + OAuth)
+ * ==============================
+ */
+
+Route::controller(OauthController::class)->group(function () {
+    Route::post('auth/google-callback', 'googleAuthentication');
+    Route::get('auth/test', [OauthController::class, 'test']);
 });
