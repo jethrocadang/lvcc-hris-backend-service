53b690089247c05cc78c6d697f012806839cc9cd
Jethro Cadang <jethrocadang@gmail.com>
2025-04-14 16:07:30 +0800

    feat[application-portal: added middleware and authentication for applicant portal

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -8,7 +8,9 @@ use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Spatie\Multitenancy\Models\Tenant;
 use Symfony\Component\HttpFoundation\Response;
 
 class JwtMiddleware
@@ -21,8 +23,22 @@ class JwtMiddleware
         try {
             Log::info('JwtMiddleware triggered', ['path' => $request->path()]);
 
+            Log::info('JWT TTL:', [
+                'value' => config('jwt.ttl'),
+                'type' => gettype(config('jwt.ttl'))
+            ]);
+
+            $token = JWTAuth::getToken();
+            $payload = JWTAuth::decode($token);
+            Log::info('Decoded JWT payload', $payload->toArray());
+            Log::info('Using DB:', ['db' => DB::connection()->getDatabaseName()]);
+            Log::info('Tenant before auth', ['tenant' => Tenant::current()?->id]);
+
             // Check if token exists & parse it
             $user = JWTAuth::parseToken()->authenticate();
+
+            Log::info('Auth DB connection:', ['connection' => optional($user)->getConnectionName()]);
+            Log::info('User not found', ['User:' => $user]);
 
             if (!$user) {
                 return response()->json(['error' => 'User not found!'], 404);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,27 +2,27 @@
 
 use App\Http\Middleware\CorsMiddleware;
 use App\Http\Middleware\JwtMiddleware;
+use App\Http\Middleware\TenantJwtMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(CorsMiddleware::class);
         $middleware
-        ->group('tenant', [
-            \Spatie\Multitenancy\Http\Middleware\NeedsTenant::class,
-            \App\Http\Middleware\EnsureTenantIsSet::class,
-        ]);
-        // TODO fix this shit!
-        // $middleware->append(JwtMiddleware::class);
-
+            ->group('tenant', [
+                \Spatie\Multitenancy\Http\Middleware\NeedsTenant::class,
+                \App\Http\Middleware\EnsureTenantIsSet::class,
+            ]);
+        $middleware->group('auth.jwt.tenant', [TenantJwtMiddleware::class]);
+        $middleware->group('auth.jwt', [JwtMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
