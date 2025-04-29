328f089a6c5d7ab018f853b7ca570ff666897861
Jethro Cadang <jethrocadang@gmail.com>
2025-04-29 20:21:09 +0800

    chore[refactor]: updated api routes for better collaboration in devlopment

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    require base_path('routes//v1/ats.php');
+    require base_path('routes/v1/ats.php');
     require base_path('routes/v1/eth.php');
     require base_path('routes/v1/hris.php');
 });
