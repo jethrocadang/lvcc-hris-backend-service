5ad7f82b03b2afe91cb9b597e2d225e620c03dec
Jethro Cadang <jethrocadang@gmail.com>
2025-05-12 23:04:11 +0800

    feat[email]: updated email, added base url to config

--- database/seeders/JobApplicationPhasesSeeder.php
+++ database/seeders/JobApplicationPhasesSeeder.php
@@ -14,6 +14,7 @@ class JobApplicationPhasesSeeder extends Seeder
 
         // Define the application phases in order
         $phases = [
+            'Verified Email',
             'Applicant Information',
             'Initial Screening',
             'Behavioral Interview',
