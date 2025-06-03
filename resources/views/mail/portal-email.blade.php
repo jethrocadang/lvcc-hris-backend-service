150f4f39393b34b038436fda9253fd0dfdba20cc
erishBRBS <erishberboso10@gmail.com>
2025-06-04 00:59:53 +0800

    portal email blade update

--- resources/views/mail/portal-email.blade.php
+++ resources/views/mail/portal-email.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -69,6 +69,170 @@
                 </table>
             </td>
         </tr>
+    </table>
+</body>
+
+</html> -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Uncial+Antiqua&display=swap" rel="stylesheet">
+    <style type="text/css">
+        html {
+            -webkit-text-size-adjust: none;
+            -ms-text-size-adjust: none;
+        }
+    </style>
+    <style em="styles">
+        @media only screen and (max-device-width:640px),
+        only screen and (max-width:640px) {
+            .em-narrow-table {
+                width: 100% !important;
+                max-width: 640px !important;
+                min-width: 320px !important;
+            }
+
+            .em-mob-wrap {
+                display: block !important;
+            }
+
+            .em-mob-width-100perc {
+                width: 100% !important;
+                max-width: 100% !important;
+                min-width: 100% !important;
+            }
+
+            .em-mob-padding_right-20 {
+                padding-right: 20px !important;
+            }
+
+            .em-mob-padding_left-20 {
+                padding-left: 20px !important;
+            }
+        }
+
+        @media screen {
+            .Roboto400 {
+                font-family: Roboto, Arial, sans-serif !important;
+                font-weight: 400 !important;
+            }
+        }
+    </style>
+</head>
+
+<body data-new-gr-c-s-check-loaded="null" data-gr-ext-installed="null" style="margin: 0; padding: 0;" data-gr-ext-disabled="forever">
+    <div class="preheader" style="color: #f0f1f3; font-size: 0px; line-height: 0px;"> <vk-snippet-end />͏‌͏͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  ͏‌  </div><!--<![endif]-->
+    <table cellpadding="0" cellspacing="0" border="0" width="100%" style="font-size: 1px; line-height: normal;">
+        <tr em="group">
+            <td align="center" bgcolor="#F8F8F8">
+
+                <table cellpadding="0" cellspacing="0" width="100%" border="0" style="max-width: 540px; min-width: 540px; width: 540px; padding: 30px" class="em-narrow-table">
+                    <tr em="block" class="em-structure">
+                    <tr>
+                        <td style="padding: 0; text-align: center; background-color: #ffffff;">
+                            <!-- Top blue bar -->
+                            <div style="height: 12px; background-color: #1f3463; border-top-left-radius: 8px; border-top-right-radius: 8px;"></div>
+
+                            <!-- Logo and School Name Side by Side -->
+                            <div style="padding: 20px 57px 20px 51px; padding-left: 30px; background-color: #ffffff;">
+                                <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" style="max-width: 640px; margin: auto;">
+                                    <tr>
+                                        <td width="100" align="center" valign="middle">
+                                            <img src="https://ci3.googleusercontent.com/mail-sig/AIorK4wi2Nvz8DzlfAFTXu-B6jFeCXHvWiS_asOQTSD3NcCabGVwlbvPATK27ZsyUByIQGoX6rEefmQnW8D7" alt="LVCC Logo" height="80" style="display: block;">
+                                        </td>
+                                        <td align="left" valign="middle" style="padding-left: 20px;">
+                                            <div style="font-family: 'Georgia', serif; font-size: 20px; line-height: 24px; color: #333333; white-space: nowrap;">
+                                                La Verdad Christian College, Inc.
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+
+                            <!-- Bottom amber and blue bars -->
+                            <div style="height: 8px; background-color: #FFBF00;"></div>
+                            <div style="height: 12px; background-color: #1f3463;"></div>
+                        </td>
+                    </tr>
+        </tr>
+        <tr em="block" class="em-structure">
+            <td align="center" style="padding: 15px 30px 106px; background-color: #ffffff;" class="em-mob-padding_left-20 em-mob-padding_right-20" bgcolor="#ffffff">
+                <table align="center" border="0" cellspacing="0" cellpadding="0" class="em-mob-width-100perc">
+                    <tr>
+                        <td width="580" valign="top" class="em-mob-wrap em-mob-width-100perc">
+                            <table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
+                                <tr>
+                                    <td style="padding: 17px 15px 10px; background-color: #ffffff; border-radius: 7px;" bgcolor="#ffffff">
+                                        <div style="font-size: 16px; line-height: 24px; color: #333333;"><span style="font-family: Georgia, serif;">Hello {{ $applicant->first_name }},</span><br><br>
+                                            <font face="Georgia, serif">Thank you for verifying your email, you can now proceed to the continuation of your application. Click the button below.</font>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr em="block" class="em-structure">
+            <td align="center" style="padding: 0px 30px; background-color: #ffffff;" class="em-mob-padding_left-20 em-mob-padding_right-20" bgcolor="#ffffff">
+                <table align="center" border="0" cellspacing="0" cellpadding="0" class="em-mob-width-100perc">
+                    <tr>
+                        <td width="580" valign="top" class="em-mob-wrap em-mob-width-100perc">
+                            <table cellpadding="0" cellspacing="0" border="0" width="100%" em="atom">
+                                <tr>
+                                    <td style="padding: 15px 0px;" align="center">
+                                        <table cellpadding="0" cellspacing="0" border="0" width="200">
+                                            <tr>
+                                                <td align="center" valign="middle" height="45" style="background-color: #1f3463; border-radius: 7px; height: 45px;">
+                                                    <a href="{{ $portalAccessUrl }}" target="_blank" style="display: block; height: 45px; font-family: Georgia, serif; color: #ffffff; font-size: 15px; line-height: 45px; text-decoration: none; white-space: nowrap;">Access Portal</a>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr em="block" class="em-structure">
+            <td align="center" valign="top" style="background: #1f3463; border-bottom-left-radius: 8px; border-bottom-right-radius: 8px;" bgcolor="#1f3463">
+                <table cellpadding="0" cellspacing="0" border="0" width="75%" style="width: 75%; min-width: 330px; border-width: 5px; border-style: solid; border-color: #FFBF00; border-left: none; border-right: none; border-bottom: none;">
+                    <tr>
+                        <td align="left">
+                            <div dir="ltr" style="display: inline-block; vertical-align: top; width: 240px;">
+                                <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                                    <tr>
+                                        <td align="left" valign="bottom" width="77">
+                                            <div style="font-family: Georgia, serif; color: #9a9a9a; font-size: 12px; line-height: 17px;" class="Roboto400">&nbsp;</div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div dir="ltr" style="display: inline-block; vertical-align: top; width: 100%; max-width: 358px;">
+                                <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                                    <tr>
+                                        <td align="left" valign="top">
+                                            <div style="font-family: Georgia, serif; color: #ffffff; font-size: 10px; line-height: 17px;" class="Roboto400">© {{ now()->year }} La Verdad Christian College, Inc. All rights reserved.</div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+    </td>
+    </tr>
     </table>
 </body>
 
